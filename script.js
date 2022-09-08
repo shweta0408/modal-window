b1a559c1187c412ed6b12f866bb6642ac0e793e1
Shweta Mishra <shmis2004@gmail.com>
2022-09-08 15:09:31 +0530

    class manipulation

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "strict mode";
 
-const modal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModel = document.querySelector(".close-modal");
 const btnsOpenModel = document.querySelectorAll(".show-modal");
 
 for (let i = 0; i < btnsOpenModel.length; i++)
-  console.log(
-    btnsOpenModel[i].addEventListener("click", function () {
-      console.log("button Clicked");
-    })
-  );
+  btnsOpenModel[i].addEventListener("click", function () {
+    console.log("Button Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
